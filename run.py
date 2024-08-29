305e45e3c623be738419010c8b611d797073f892
Vishaalcodre <vishaa369@gmail.com>
2024-08-29 18:59:20 +0530

    Jinja Part 1

--- run.py
+++ run.py
@@ -25,9 +25,47 @@ def numbers(number=5):
     return "<h1>The number is: {}</h1>".format(number)
 
 
-@app.route('/temp/')
-def html():
-    return render_template("hello.html")
+# strings
+@app.route('/text/<string:name>')
+def working_with_strings(name):
+    return '<h1> here is a string: ' + name + '</h1>'
+
+
+# numbers
+@app.route('/numbers/<int:num>')
+def working_with_numbers(num):
+    return '<h1> the number you picked is: ' + str(num) + '</h1>'
+
+
+# add numbers
+@app.route('/add/<int:num1>/<int:num2>')
+def adding_integers(num1, num2):
+    return '<h1> the sum is : {}'.format(num1 + num2) + '</h1>'
+
+
+# floats
+@app.route('/product/<float:num1>/<float:num2>')
+def product_two_numbers(num1, num2):
+    return '<h1> the product is : {}'.format(num1 * num2) + '</h1>'
+
+
+# rendering templates
+@app.route('/temp')
+def using_templates():
+    return render_template('hello.html')
+
+
+# JINJA TEMPLATES
+@app.route('/watch')
+def top_movies():
+    movie_list = ['autopsy of jane doe',
+                  'neon demon',
+                  'ghost in a shell',
+                  'kong: skull island',
+                  'john wick 2',
+                  'spiderman - homecoming']
+
+    return render_template("hello.html", movies=movie_list, name="Praveen")
 
 if __name__ == "__main__":
     app.run(debug=True)

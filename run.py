20c27d43479a8eeee1b5ab52c515caf5fa60e164
Vishaalcodre <vishaa369@gmail.com>
2024-09-03 19:49:20 +0530

    Creating table and relations

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -121,6 +122,7 @@ def jinja_macros():
 
     return render_template('using_macros.html', movies=movie_dict)
 
+#Publication Table
 class Publication(db.Model):
     __tablename__ = 'publication'
 
@@ -134,6 +136,33 @@ class Publication(db.Model):
     def __repr__(self):
         return "The id is {} and the Name is {}".format(self.id, self.name)
 
+#Book Table
+class Book(db.Model):
+    __tablename__ = 'book'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500), nullable=False, index=True)
+    author = db.Column(db.String(350))
+    avg_rating = db.Column(db.Float)
+    format = db.Column(db.String(50))
+    image = db.Column(db.String(100), unique=True)
+    num_pages = db.Column(db.Integer)
+    pub_date = db.Column(db.DateTime, default=datetime.now())
+
+    pub_id = db.Column(db.Integer, db.ForeignKey('publication.id'))
+
+    def __init__(self, title, author, avg_rating, book_format, image, num_pages, pub_id):
+
+        self.title = title
+        self.author = author
+        self.avg_rating = avg_rating
+        self.format = book_format
+        self.image = image
+        self.num_pages = num_pages
+        self.pub_id = pub_id
+
+    def __repr__(self):
+        return '{} by {}'.format(self.title, self.author)
 
 if __name__ == "__main__":
     db.create_all()

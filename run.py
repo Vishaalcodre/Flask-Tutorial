cf1ee7835a3ac3341af3e09469c4174642f28ccb
Vishaalcodre <vishaa369@gmail.com>
2024-09-03 20:10:56 +0530

    Inserting Records

--- run.py
+++ run.py
@@ -129,8 +129,7 @@ class Publication(db.Model):
     id = db.Column(db.Integer ,primary_key= True)
     name = db.Column(db.String(80), nullable= False)
 
-    def __init__(self, id, name):
-        self.id = id
+    def __init__(self, name):
         self.name = name
 
     def __repr__(self):
